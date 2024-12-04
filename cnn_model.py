a680a25f8aa33ee238b7e3edf9602eaf025368ce
snelson59 <snelson59@huskers.unl.edu>
2024-12-04 21:09:46 +0000

    edited imports and matrix size

--- cnn_model.py
+++ cnn_model.py
@@ -1,31 +1,36 @@
 from sklearn.model_selection import train_test_split
-from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.python.keras.engine import sequential
-from tensorflow.python.keras.utils import to_categorical
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential
 import matplotlib.pyplot as plt
 import numpy as np
 
 def cnn_model():
 
 #loading dataset
+    
     data = np.load('Matrices.npy')
     labels = np.load('Labels.npy')
-    #check if data = grayscale 150x150 and labels = 1 dimensional with class labels
+
+#checks if data = grayscale 200x200 and labels = 1 dimensional with class labels
+    
     print(f"Data shape: {data.shape}")
     print(f"Labels shape: {labels.shape}")
+
     # Flatten the 2D matrices to 1D arrays for the model
-    data = data.reshape(data.shape[0], 150, 150, 1)
+    
+    data = data.reshape(data.shape[0], 200, 200, 1)
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
 #exploring data
 
-    plt.imshow(X_train[0].reshape(150, 150), cmap="gray")
+    plt.imshow(X_train[0].reshape(200, 200), cmap="gray")
     plt.show()
 
 #building the model
 
-    model = sequential.Sequential()
-    model.add(Conv2D(32,kernel_size=3, activation='relu',input_shape=(150,150,1)))
+    model = Sequential()
+    model.add(Conv2D(32,kernel_size=3, activation='relu',input_shape=(200,200,1)))
     model.add(Conv2D(64,kernel_size=3,activation='relu'))
     model.add(Conv2D(128,kernel_size=3,activation='relu'))
     model.add(Flatten())

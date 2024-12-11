a9f0c14cf38e174312a47d9ab929168bf8347527
Thomas Anderson <anderson.tom777@gmail.com>
2024-12-11 12:36:57 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function stopDrawing() {
 
 function draw(e) {
     if (!drawing) return;
-    ctx.lineWidth = 5;
+    ctx.lineWidth = 2;  // Thinner line width
     ctx.lineCap = 'round';
     ctx.strokeStyle = 'black';
 
